eeb4b0524f1a65701a049df5d0961ee13aec5763
xmchenph <xmchenph@gmail.com>
2017-10-21 23:46:14 +0800

    userprofile

--- login.php
+++ login.php
@@ -6,7 +6,7 @@ session_start();
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Tasource - Making task sourcing simple</title>
+    <title>PetCare</title>
     <link rel="stylesheet" type="text/css" href="./vendor/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="./vendor/css/style.css">
     <link rel="stylesheet" type="text/css" href="./vendor/css/login-styling.css">
@@ -15,80 +15,87 @@ session_start();
     <script src="./vendor/bootstrap/js/bootstrap.min.js"></script>
 </head>
 <body>
-    <!-- include php -->
-    <?php include "config/db-connection.php"; ?>
+<!-- include php -->
+<?php include "config/db-connection.php"; ?>
 
-    <div class="main-container">
-        <div class="container">
-            <div class="wrapper">
-                <div class="col-lg-4 col-md-6 col-sm-6 col-xs-8 panel panel-default ogin-panel">
+<div class="main-container">
+    <div class="container">
+        <div class="wrapper">
+            <div class="col-lg-4 col-md-6 col-sm-6 col-xs-8 panel panel-default ogin-panel">
 
-                    <!-- heading -->
-                    <div class="panel-title login-panelheading">
-                        <h4>Welcome</h4>
-                        <h5 class="login-panel-subtitle">Please login here.</h5>
-                    </div>
+                <!-- heading -->
+                <div class="panel-title login-panelheading">
+                    <h4>Welcome</h4>
+                    <h5 class="login-panel-subtitle">Please login here.</h5>
+                </div>
 
-                    <!-- body -->
-                    <div class="panel-body">
+                <!-- body -->
+                <div class="panel-body">
 
-                        <!-- login form -->
-                        <form class="login-form">
+                    <!-- login form -->
+                    <form class="login-form">
 
-                            <!-- email -->
-                            <div class="row email-input-row">
-                                <div class="col-lg-5 col-sm-5 email-label">
-                                    <h5>Email</h5>
-                                </div>
-                                <div class="col-lg-7 col-sm-7 email-input">
-                                    <input class="form-control" name="email" type="email"></input>    
-                                </div>
+                        <!-- email -->
+                        <div class="row email-input-row">
+                            <div class="col-lg-5 col-sm-5 email-label">
+                                <h5>Email</h5>
                             </div>
-
-                            <!-- password -->
-                            <div class="row password-input-row">
-                                <div class="col-lg-5 col-sm-5 password-label">
-                                    <h5>Password</h5>
-                                </div>
-                                <div class="col-lg-7 col-sm-7 password-input">
-                                    <input class="form-control" name="password" type="password"></input>
-                                </div>
+                            <div class="col-lg-7 col-sm-7 email-input">
+                                <input class="form-control" name="email" type="email"></input>
                             </div>
+                        </div>
 
-                            <!-- login button -->
-                            <div class="row login-button-wrapper">
-                                <div class="col-lg-6 col-md-6 col-xs-12">
-                                    <button type="submit" name="login" class="btn btn-large btn-success login-button">Sign in</button>
-                                </div>
-                                <div class="col-lg-6 col-md-6 col-xs-12">
-                                    <button type="button" class="btn btn-large btn-primary login-button" onclick="window.location='signup.php'">Sign Up</button>
-                                </div>
+                        <!-- password -->
+                        <div class="row password-input-row">
+                            <div class="col-lg-5 col-sm-5 password-label">
+                                <h5>Password</h5>
                             </div>
-                        </form>
+                            <div class="col-lg-7 col-sm-7 password-input">
+                                <input class="form-control" name="password" type="password"></input>
+                            </div>
+                        </div>
 
-                        <?php if(isset($_GET['login'])) {
-                            $query = "SELECT u.user_id
+                        <!-- login button -->
+                        <div class="row login-button-wrapper">
+                            <div class="col-lg-6 col-md-6 col-xs-12">
+                                <button type="submit" name="login" class="btn btn-large btn-success login-button">Sign
+                                    in
+                                </button>
+                            </div>
+                            <div class="col-lg-6 col-md-6 col-xs-12">
+                                <button type="button" class="btn btn-large btn-primary login-button"
+                                        onclick="window.location='signup.php'">Sign Up
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+
+                    <?php if (isset($_GET['login'])) {
+                        $email = $_GET['email'];
+                        $query = "SELECT u.user_id
                                       FROM pet_user u
-                                      WHERE u.email = '".$_GET['email']."'
-                                        AND u.password = '".$_GET[password]."';";
-                            $result = pg_query($query) or die('Query failed: ' . pg_last_error());
+                                      WHERE u.email = '" . $email . "'
+                                        AND u.password = '" . $_GET['password'] . "';";
+                        $result = pg_query($query) or die('Query failed: ' . pg_last_error());
 
-                            $row = pg_fetch_row($result);
-                            if ($row) {
-                                $_SESSION["user_id"] = $row[0];
-                                header("Location: userprofile.php");
-                                exit;
-                            } else {
-                                echo "Incorrect email or password";
-                            }
-
-                            pg_free_result($result);
+                        $row = pg_fetch_row($result);
+                        if ($row) {
+                            $user_id = $row[0];
+                            $_SESSION["user_email"] = $email;
+                            $_SESSION["user_id"] = $user_id;
+                            header("Location: userprofile.php");
+                            exit;
+                        } else {
+                            echo "Incorrect email or password";
                         }
-                        ?>
-                    </div>
+
+                        pg_free_result($result);
+                    }
+                    ?>
                 </div>
             </div>
         </div>
     </div>
+</div>
 </body>
 </html>
--- userprofile.php
+++ userprofile.php
@@ -12,7 +12,7 @@ if (isset($_SESSION["user_id"])) {
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Tasource - Making task sourcing simple</title>
+    <title>PetCare</title>
     <link rel="stylesheet" type="text/css" href="./vendor/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="./vendor/css/style.css">
     <link rel="stylesheet" type="text/css" href="./vendor/css/new-task-styling.css">
@@ -25,85 +25,86 @@ if (isset($_SESSION["user_id"])) {
     <script src="./vendor/js/find-task.js"></script>
 </head>
 <body>
-    <!-- include php -->
-    <?php include "config/db-connection.php"; ?>
+<!-- include php -->
+<?php include "config/db-connection.php"; ?>
+<?php $query = "SELECT *
+          FROM pet_user u
+          WHERE u.user_id = $user_id";
+$result = pg_query($query) or die('Query failed: ' . pg_last_error());
 
-    <!-- navigation bar -->
-    <nav class="navbar navbar-inverse navigation-bar navbar-fixed-top">
-        <div class="container navbar-container">
-            <div class="navbar-header pull-left"><a class="navbar-brand" href="">PetPet</a></div>
-            <div class="nav navbar-nav navbar-form">
-                <div class="input-icon">
-                    <i class="glyphicon glyphicon-search search"></i>
-                    <input type="text" placeholder="Type to search..." class="form-control search-form" tabindex="1">
-                </div>
-            </div>
-            <div class="collapse navbar-collapse pull-right">
-                <ul class="nav navbar-nav">
-                    <li><a href="userprofile.php">View profile</a></li>
-                    <li><a href="request.php">Send Requests</a></li>
+$row = pg_fetch_row($result);
+$user_name = $row[1];
+?>
 
-                </ul>
+<!--navigation bar-->
+<nav class="navbar navbar-inverse navigation-bar navbar-fixed-top">
+    <div class="container navbar-container">
+        <div class="navbar-header pull-left"><a class="navbar-brand" href=""> PetCare</a></div>
+        <div class="nav navbar-nav navbar-form">
+            <div class="input-icon">
+                <i class="glyphicon glyphicon-search search"></i>
+                <input type="text" placeholder="Type to search..." class="form-control search-form" tabindex="1">
             </div>
         </div>
-    </nav>
-
-
+        <div class="collapse navbar-collapse pull-right">
+            <ul class="nav navbar-nav">
+                <li><a href="request.php"> Send Request </a></li>
+                <li><a href="history.php"> View History </a></li>
+                <li><a href="logout.php"> Log Out </a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<div class="content-container container">
+    <!-- page content -->
     <div class="container-fluid">
 
         <!-- panel -->
         <div class="panel new-task-panel">
-            <form action="" id="findForm">
-                <!-- panel heading -->
-                <div class="panel-heading">
-                    <h2 class="new-task-form-title">Your Pets and its takers</h2>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="container">
+                        <div>
+                            <h2>User's name</h2>
+                        </div>
+                        <table>
+                            <tr>
+                                <th>Email:</th>
+                                <td>email@email.com</td>
+                            </tr>
+                            <tr>
+                                <th>Home address:</th>
+                                <td>address, address road, address city</td>
+                            </tr>
+                        </table>
+                    </div>
                 </div>
-
-                <!-- panel body -->
-                <div class="panel-body">
-
-
-                    <table class="table table-striped">
-
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="container">
+                        <h3>Your pets</h3>
+                    </div>
+                    <table>
                         <tr>
-                            <th>Pet ID</th>
-                            <th>Pet Name</th>
-                            <th>Pet Size</th>
-                            <th>Pet Age</th>
+                            <!-- Add sorting features -->
+                            <th>
+                                No.
+                            </th>
+                            <th>
+                                pet size
+                            </th>
+                            <th>
+                                pet age
+                            </th>
+                            <th>
+                                pet name
+                            </th>
                         </tr>
-
-
-                        <?php
-                        $query = "SELECT * FROM pet p WHERE p.owner_id =$user_id;";
-                        $result = pg_query($query) or die('Query failed: ' . pg_last_error());
-
-
-                        while ($row = pg_fetch_row($result)) {
-
-                            $pet_id = $row[0];
-                            $pet_name = pg_fetch_row(pg_query("SELECT name FROM petcategory WHERE pcat_id = " . $row[2] . ";"))[0];
-                            $pet_size = pg_fetch_row(pg_query("SELECT size FROM petcategory WHERE pcat_id = " . $row[2] . ";"))[0];
-                            $pet_age = pg_fetch_row(pg_query("SELECT age FROM petcategory WHERE pcat_id = " . $row[2] . ";"))[0];
-
-                            echo "<tr>";
-                            echo "<td >$pet_id</td >";
-                            echo "<td >$pet_name</td >";
-                            echo "<td >$pet_size</td >";
-                            echo "<td >$pet_age</td >";
-                            echo "</tr>";
-                        }
-                        ?>
-
-
+                        <
                     </table>
-
                 </div>
-
-            </form>
+            </div>
         </div>
     </div>
-
-
-
-</body>>
-
+</body>
