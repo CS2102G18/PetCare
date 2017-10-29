e15ea1c7235adf9917f471dd16fa33973592457b
peiyixie <31428921+peiyixie@users.noreply.github.com>
2017-10-29 16:22:11 +0800

    updated request page

--- Alpha/request.php
+++ Alpha/request.php
@@ -4,7 +4,7 @@ session_start();
 if (isset($_SESSION["user_id"])) {
     $user_id = $_SESSION["user_id"];
 } else {
-    header("Location: login.php");
+    header("Location: ../login.php");
     exit;
 }
 ?>
@@ -13,16 +13,16 @@ if (isset($_SESSION["user_id"])) {
 <html>
 <head>
     <title>PetCare</title>
-    <link rel="stylesheet" type="text/css" href="./vendor/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="./vendor/css/style.css">
-    <link rel="stylesheet" type="text/css" href="./vendor/css/new-task-styling.css">
-    <link rel="stylesheet" href="./vendor/css/bootstrap-datetimepicker.min.css">
+    <link rel="stylesheet" type="text/css" href="../vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../vendor/css/style.css">
+    <link rel="stylesheet" type="text/css" href="../vendor/css/new-task-styling.css">
+    <link rel="stylesheet" href="../vendor/css/bootstrap-datetimepicker.min.css">
 
-    <script src="./vendor/js/jquery-3.2.0.min.js"></script>
-    <script src="./vendor/bootstrap/js/bootstrap.min.js"></script>
-    <script src="./vendor/js/jquery.ns-autogrow.min.js"></script>
-    <script src="./vendor/js/bootstrap-datetimepicker.min.js"></script>
-    <script src="./vendor/js/find-task.js"></script>
+    <script src="../vendor/js/jquery-3.2.0.min.js"></script>
+    <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="../vendor/js/jquery.ns-autogrow.min.js"></script>
+    <script src="../vendor/js/bootstrap-datetimepicker.min.js"></script>
+    <script src="../vendor/js/find-task.js"></script>
     <script type="text/javascript">
         $(document).ready(function () {
             $("#successmodal").modal('show');
@@ -30,10 +30,10 @@ if (isset($_SESSION["user_id"])) {
     </script>
 </head>
 <body>
-<?php include "config/db-connection.php"; ?>
+<?php include "../config/db-connection.php"; ?>
 <nav class="navbar navbar-inverse navigation-bar navbar-fixed-top">
     <div class="container navbar-container">
-        <div class="navbar-header pull-left"><a class="navbar-brand" href="userprofile.php"> PetCare</a></div>
+        <div class="navbar-header pull-left"><a class="navbar-brand" href="../userprofile.php"> PetCare</a></div>
         <div class="nav navbar-nav navbar-form">
             <div class="input-icon">
                 <i class="glyphicon glyphicon-search search"></i>
@@ -43,8 +43,8 @@ if (isset($_SESSION["user_id"])) {
         <div class="collapse navbar-collapse pull-right">
             <ul class="nav navbar-nav">
                 <li><a href="request.php"> Send Request </a></li>
-                <li><a href="history.php"> View History </a></li>
-                <li><a href="logout.php"> Log Out </a></li>
+                <li><a href="../history.php"> View History </a></li>
+                <li><a href="../logout.php"> Log Out </a></li>
             </ul>
         </div>
     </div>
