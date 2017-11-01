6fbb8b5c4dd25ac4284ea91732b016d9613c0af6
XiaRui1996 <xr2739401611@gmail.com>
2017-11-01 17:55:10 +0800

    change ui

--- owner.php
+++ owner.php
@@ -15,8 +15,10 @@ if (isset($_SESSION["user_id"])) {
     <title>PetCare</title>
     <link rel="stylesheet" type="text/css" href="./vendor/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="./vendor/css/style.css">
-    <link rel="stylesheet" type="text/css" href="./vendor/css/new-task-styling.css">
     <link rel="stylesheet" href="./vendor/css/bootstrap-datetimepicker.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <script src="./vendor/js/jquery-3.2.0.min.js"></script>
     <script src="./vendor/bootstrap/js/bootstrap.min.js"></script>
@@ -35,24 +37,14 @@ if (isset($_SESSION["user_id"])) {
 <body>
 <!-- include php -->
 <?php include "config/db-connection.php"; ?>
-<?php
 
-$query1 = "SELECT name, email, address FROM pet_user WHERE user_id = $user_id";
-$result1 = pg_query($query1) or die('Query failed: ' . pg_last_error());
-
-$row = pg_fetch_row($result1);
-$user_name = $row[0];
-$user_email = $row[1];
-$user_address = $row[2];
-
-?>
 
 
 <!--navigation bar-->
-<nav class="navbar navbar-inverse navigation-bar navbar-fixed-top navbar-owner">
+<nav class="navbar navigation-bar navbar-inverse navbar-owner navbar-fixed-top">
     <div class="container navbar-container">
         <div class="navbar-header pull-left"><a class="navbar-brand" href=""> PetCare</a></div>
-        <div class="collapse navbar-collapse pull-right">
+        <div class="collapse pull-right navbar-collapse">
             <ul class="nav navbar-nav">
                 <li><a href="request.php"> Send Request </a></li>
                 <li><a href="history.php"> View History </a></li>
@@ -66,24 +58,28 @@ $user_address = $row[2];
     <div class="container-fluid">
 
         <!-- panel -->
-        <div class="panel new-task-panel">
+        <div class="panel">
             <div class="row">
                 <div class="col-md-12">
                     <div class="container">
+                        <?php
+
+                        $query1 = "SELECT name, email, address FROM pet_user WHERE user_id = $user_id";
+                        $result1 = pg_query($query1) or die('Query failed: ' . pg_last_error());
+
+                        $row = pg_fetch_row($result1);
+                        $user_name = $row[0];
+                        $user_email = $row[1];
+                        $user_address = $row[2];
+
+                        ?>
 
                         <div>
                             <h2>Hello, <?php echo $user_name; ?> </h2>
                         </div>
                         <table>
-
-                            <tr>
-                                <th>Email:</th>
-                                <td><?php echo $user_email; ?></td>
-                            </tr>
-                            <tr>
-                                <th>Address:</th>
-                                <td><?php echo $user_address; ?></td>
-                            </tr>
+                            <tr><th>Email:</th><td><?php echo $user_email; ?></td></tr>
+                            <tr><th>Address:</th><td><?php echo $user_address; ?></td></tr>
                         </table>
                     </div>
                 </div>
@@ -105,7 +101,7 @@ $user_address = $row[2];
                     </div>
 
                     <div class="container">
-                        <h4>Ongoing Requests</h4>
+                        <i class="glyphicon glyphicon-ok-circle" style="font-size:20px;"><p>  Ongoing Requests  </p></i>
                     </div>
 
 
@@ -158,10 +154,11 @@ $user_address = $row[2];
 
 
                     <div class="container">
-                        <h4>Unsuccessful Requests</h4>
+                        <i class="glyphicon glyphicon-remove-circle" style="font-size:20px;"><p>  Unsuccessful Requests </p></i>
                     </div>
 
                     <table class="table table-striped">
+
 
                         <tr>
                             <th>Taker Name</th>
@@ -174,7 +171,7 @@ $user_address = $row[2];
 
 
                         <?php
-                        $query4 = "SELECT u.name, r.care_begin, r.care_end, r.bids, p.pet_name FROM request r, pet_user u, pet p 
+                        $query4 = "SELECT u.name, r.care_begin, r.care_end, r.bids, p.pet_name, r.request_id FROM request r, pet_user u, pet p 
                                    WHERE r.owner_id = $user_id 
                                          AND r.status = 'failed' 
                                          AND r.taker_id = u.user_id
@@ -190,17 +187,18 @@ $user_address = $row[2];
                             $care_end = $row[2];
                             $bids = $row[3];
                             $request_pet_name = $row[4];
+                            $request_id = $row[5];
 
 
-                            echo "<tr>";
+                            echo "<tr >";
                             echo "<td >$taker_name</td >";
                             echo "<td >$request_pet_name</td >";
                             echo "<td >$care_begin</td >";
                             echo "<td >$care_end</td >";
                             echo "<td >$bids</td >";
                             echo "<td>
-                                        <form class='form-inline' action='request.php' method='get'><div class='form-group' style='float: right;'><input type='submit' class='form-control' value='Request Another Taker'></div><input type='hidden' name='request_id' value=$request_id></form>
-                                        <form class='form-inline' action='read.php' method='get'><div class='form-group' style='float: right;'><input type='submit' class='form-control' value='Read'></div><input type='hidden' name='request_id' value=$request_id></form>                                                                                                                                                                                                            
+                                        <a class=\"btn btn-danger\" role=\"button\" href=\"read.php?request_id=$request_id\">Read</a>
+                                        <a class=\"btn btn-default\" role=\"button\" href=\"request.php\">Request Again</a>
                                   </td>";
                             echo "</tr>";
                             };
@@ -212,6 +210,7 @@ $user_address = $row[2];
                     <br/>
                     <br/>
                     <div class="container">
+                        <i class="material-icons" style="font-size:36px">pets</i>
                         <h3>Your pets</h3>
                     </div>
 
@@ -240,7 +239,7 @@ $user_address = $row[2];
                             $pet_size = pg_fetch_row(pg_query("SELECT size FROM petcategory WHERE pcat_id = " . $row[2] . ";"))[0];
                             $pet_age = pg_fetch_row(pg_query("SELECT age FROM petcategory WHERE pcat_id = " . $row[2] . ";"))[0];
 
-                            echo "<tr>";
+                            echo "<tr >";
                             echo "<td >$pet_id</td >";
                             echo "<td >$pet_name</td >";
                             echo "<td >$pet_species</td >";
@@ -264,6 +263,3 @@ $user_address = $row[2];
     </div>
 </body>
 </html>
-<?php
-
-?>
