8572c8339c562aeea694ab58b3984ba311cdf2a9
xmchenph <xmchenph@gmail.com>
2017-11-03 00:04:30 +0800

    admin pet search

--- admin/admin_pet.php
+++ admin/admin_pet.php
@@ -64,105 +64,129 @@ if (isset($_SESSION["user_id"])) {
     </div>
     <div class="container-fluid">
         <div class="panel new-task-panel">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="col-sm-3">
-                        <label for="pet_name">Pet's Name</label>
-                        <input id="pet_name" type="text" class="form-control" placeholder="Pet Name"
-                               required="true">
-                    </div>
-                    <div class="col-sm-3">
-                        <label for="pet_species">Pet's Species</label>
-                        <select id="pet_species" class="form-control" required="true">
-                            <option value="">Select Category</option>
-                            <?php
-                            $query = "SELECT DISTINCT species FROM petcategory";
-                            $result = pg_query($query) or die('Query failed: ' . pg_last_error());
-                            while ($row = pg_fetch_row($result)) {
-                                echo "<option value='" . $row[0] . "'>" . $row[0] . "</option><br>";
-                            }
-                            pg_free_result($result);
-                            ?>
-                        </select>
-                    </div>
-                    <div class="col-sm-2">
-                        <label for="pet_age">Pet's Age</label>
-                        <select id="pet_age" class="form-control" required="true">
-                            <option value="">Select Age</option>
-                            <?php
-                            $query = "SELECT DISTINCT age FROM petcategory";
-                            $result = pg_query($query) or die('Query failed: ' . pg_last_error());
-                            while ($row = pg_fetch_row($result)) {
-                                echo "<option value='" . $row[0] . "'>" . $row[0] . "</option><br>";
-                            }
-                            pg_free_result($result);
-                            ?>
-                        </select>
-                    </div>
-                    <div class="col-sm-2">
-                        <label for="pet_size">Pet's Size</label>
-                        <select name="pet_size" class="form-control" required="true">
-                            <option value="">Select Size</option>
-                            <?php
-                            $query = "SELECT DISTINCT size FROM petcategory";
-                            $result = pg_query($query) or die('Query failed: ' . pg_last_error());
-                            while ($row = pg_fetch_row($result)) {
-                                echo "<option value='" . $row[0] . "'>" . $row[0] . "</option><br>";
-                            }
-                            pg_free_result($result);
-                            ?>
-                        </select>
-                    </div>
-                    <div class="col-sm-2">
-                        <label for="search"></label>
-                        <a class="btn btn-primary" role="button" name="search">Search</a>
+            <form action="" id="findForm">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="col-sm-3">
+                            <label for="pet_kw">Pet's Name</label>
+                            <input id="pet_kw" name="pet_kw" type="text" class="form-control" placeholder="Keywords">
+                        </div>
+                        <div class="col-sm-3">
+                            <label for="pet_species">Pet's Species</label>
+                            <select id="pet_species" name="pet_species" class="form-control">
+                                <option value="">Select Category</option>
+                                <?php
+                                $query = "SELECT DISTINCT species FROM petcategory";
+                                $result = pg_query($query) or die('Query failed: ' . pg_last_error());
+                                while ($row = pg_fetch_row($result)) {
+                                    echo "<option value='" . $row[0] . "'>" . $row[0] . "</option><br>";
+                                }
+                                pg_free_result($result);
+                                ?>
+                            </select>
+                        </div>
+                        <div class="col-sm-2">
+                            <label for="pet_age">Pet's Age</label>
+                            <select id="pet_age" name="pet_age" class="form-control">
+                                <option value="">Select Age</option>
+                                <?php
+                                $query = "SELECT DISTINCT age FROM petcategory";
+                                $result = pg_query($query) or die('Query failed: ' . pg_last_error());
+                                while ($row = pg_fetch_row($result)) {
+                                    echo "<option value='" . $row[0] . "'>" . $row[0] . "</option><br>";
+                                }
+                                pg_free_result($result);
+                                ?>
+                            </select>
+                        </div>
+                        <div class="col-sm-2">
+                            <label for="pet_size">Pet's Size</label>
+                            <select name="pet_size" id="pet_size" class="form-control">
+                                <option value="">Select Size</option>
+                                <?php
+                                $query = "SELECT DISTINCT size FROM petcategory";
+                                $result = pg_query($query) or die('Query failed: ' . pg_last_error());
+                                while ($row = pg_fetch_row($result)) {
+                                    echo "<option value='" . $row[0] . "'>" . $row[0] . "</option><br>";
+                                }
+                                pg_free_result($result);
+                                ?>
+                            </select>
+                        </div>
+                        <div class="col-sm-2">
+                            <input type="submit" class="btn-success btn" id="findBtn" name="add" value="Add">
+                            <input type="submit" class="btn-primary btn" id="findBtn" name="search" value="Search">
+                            <a href="admin_pet.php" class="btn-default btn">Cancel</a>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <br><br>
-            <div class="row">
-                <div class="col-md-12">
-                    <table class="table table-striped">
-                        <tr>
-                            <th>Pet ID</th>
-                            <th>Pet Name</th>
-                            <th>Pet Species</th>
-                            <th>Pet Size</th>
-                            <th>Pet Age</th>
-                            <th>Actions</th>
-                        </tr>
-                        <?php
-                        if (!isset($_GET['search'])) {
-                            $query2 = "SELECT * FROM pet p ORDER BY pets_id;";
-                        } else {
-                            $pet_kw = $_GET['pet_name'];
-                            $pet_species = $_GET[''];
-                        }
-                        $result2 = pg_query($query2) or die('Query failed: ' . pg_last_error());
-                        while ($row = pg_fetch_row($result2)) {
-                            $pet_id = $row[0];
-                            $pet_name = $row[3];
-                            $pet_species = pg_fetch_row(pg_query("SELECT species FROM petcategory WHERE pcat_id = " . $row[2] . ";"))[0];
-                            $pet_size = pg_fetch_row(pg_query("SELECT size FROM petcategory WHERE pcat_id = " . $row[2] . ";"))[0];
-                            $pet_age = pg_fetch_row(pg_query("SELECT age FROM petcategory WHERE pcat_id = " . $row[2] . ";"))[0];
+                <br><br>
+                <div class="row">
+                    <div class="col-md-12">
+                        <table class="table table-striped">
+                            <tr>
+                                <th>Pet ID</th>
+                                <th>Pet Name</th>
+                                <th>Pet Species</th>
+                                <th>Pet Size</th>
+                                <th>Pet Age</th>
+                                <th>Actions</th>
+                            </tr>
+                            <?php
+                            if (isset($_GET['search'])) {
+                                $pet_kw = $_GET['pet_kw'];
+                                $pet_species = $_GET['pet_species'];
+                                $pet_age = $_GET['pet_age'];
+                                $pet_size = $_GET['pet_size'];
 
-                            echo "<tr>";
-                            echo "<td >$pet_id</td >";
-                            echo "<td >$pet_name</td >";
-                            echo "<td >$pet_species</td >";
-                            echo "<td >$pet_size</td>";
-                            echo "<td >$pet_age</td >";
-                            echo "<td >
-                      <a class=\"btn btn-default\" role=\"button\" href=\"../editpet.php?p_id=$pet_id\">Edit</a>
-                      <a class=\"btn btn-danger\" role=\"button\" href=\"../deletepet.php?pet_id=$pet_id\">Delete</a>
-                      </td>";
-                            echo "</tr>";
-                        }
-                        pg_free_result($result2);
-                        ?>
-                    </table>
+                                $query = "SELECT p.pets_id, p.pet_name, pc.species, pc.size, pc.age
+                                          FROM pet p INNER JOIN petcategory pc ON p.pcat_id = pc.pcat_id
+                                          WHERE 1 = 1 ";
+
+                                if (trim($pet_kw)) {
+                                    $query .= " AND UPPER(p.pet_name) LIKE UPPER('%" . $pet_kw . "%')";
+                                }
+
+                                if (trim($pet_age)) {
+                                    $query .= " AND pc.age = '" . $pet_age . "'";
+                                }
+
+                                if (trim($pet_species)) {
+                                    $query .= " AND pc.species = '" . $pet_species . "'";
+                                }
+
+                                if (trim($pet_size)) {
+                                    $query .= " AND pc.size = '" . $pet_size . "'";
+                                }
+                                $query .= ";";
+
+                                $result = pg_query($query) or die('Query failed1: ' . pg_last_error());
+                            } else {
+                                $query = "SELECT p.pets_id, p.pet_name, pc.species, pc.size, pc.age
+                                          FROM pet p INNER JOIN petcategory pc ON p.pcat_id = pc.pcat_id;";
+                                $result = pg_query($query) or die('Query failed2: ' . pg_last_error());
+                            }
+
+                            while ($row = pg_fetch_row($result)) {
+                                $pet_id = $row[0];
+                                echo "<tr>";
+                                echo "<td >$row[0]</td >";
+                                echo "<td >$row[1]</td >";
+                                echo "<td >$row[2]</td >";
+                                echo "<td >$row[3]</td>";
+                                echo "<td >$row[4]</td >";
+                                echo "<td >
+                                <a class=\"btn btn-default\" role=\"button\" href=\"../editpet.php?p_id=$pet_id\">Edit</a>
+                                <a class=\"btn btn-danger\" role=\"button\" href=\"../deletepet.php?pet_id=$pet_id\">Delete</a>
+                                </td>";
+                                echo "</tr>";
+                            }
+                            pg_free_result($result2);
+                            ?>
+                        </table>
+                    </div>
                 </div>
-            </div>
+            </form>
         </div>
     </div>
 </div>
