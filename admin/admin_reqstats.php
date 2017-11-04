0b731b7ef7501129c7269a5bd6e98816658f2e8c
XiaRui1996 <xr2739401611@gmail.com>
2017-11-04 20:25:06 +0800

    edit long sql

--- admin/admin_reqstats.php
+++ admin/admin_reqstats.php
@@ -103,123 +103,121 @@ if (isset($_SESSION["user_id"])) {
                                     <th>Pet Species</th>
                                     <th>Taker Name</th>
                                     <th>Taker Email</th>
-                                    <th>Average Bids Provided</th>
-                                    <th>Number of Successful Assignments Done</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                <?php
-                                $query2 = "SELECT u.name, u.email, k.average, k.num
-                       FROM (SELECT r.taker_id AS id, AVG(r.bids) AS average, COUNT(r.request_id) AS num
-                             FROM request r
+                                    <th >Average Bids／Hour</th>
+                            <th >Total Number of Hours Completed</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php
+                        $query2 = "
+                        SELECT u.name, u.email, k.average, k.num
+                        FROM (SELECT r.taker_id AS id, (SUM(r.bids)/SUM(r.totaltime)*60) AS average, (SUM(r.totaltime)/60) AS num
+                             FROM request r WHERE r.status = 'successful'
                              GROUP BY r.taker_id) AS k, pet_user u
-                       WHERE u.user_id = k.id AND NOT EXISTS(SELECT *
-                                                             FROM (SELECT AVG(r1.bids) AS avg FROM request r1 GROUP BY r1.taker_id) AS k1 
-                                                             WHERE k.average < k1.avg);";
+                        WHERE u.user_id = k.id AND NOT EXISTS(SELECT *
+                                                              FROM (SELECT (SUM(r1.bids)/SUM(r1.totaltime)*60) AS avg FROM request r1 GROUP BY r1.taker_id) AS k1 
+                                                              WHERE k.average < k1.avg);";
 
-                                $result2 = pg_query($query2) or die('Query failed: ' . pg_last_error());
-                                $row2 = pg_fetch_row($result2);
+                        $result2 = pg_query($query2) or die('Query failed: ' . pg_last_error());
+                        $row2 = pg_fetch_row($result2);
 
-                                $average2 = $row2[2] < 0 ? '' : round(floatval($row2[2]), 2);
-                                echo "<tr>
+                        $average2 = $row2[2] < 0 ? '' : round(floatval($row2[2]), 2);
+                        $totaltime2 = $row2[3] < 0 ? '' : round(floatval($row2[3]), 2);
+                        echo "<tr>
                   <td>All</td>
                   <td>$row2[0]</td>
                   <td>$row2[1]</td>
                   <td>$average2</td>
-                  <td>$row2[3]</td>
+                  <td>$totaltime2</td>
 
                   </tr>";
-                                $query4 = "SELECT k.species, u.name, u.email, k.average, k.num
-                       FROM (SELECT r.taker_id AS id, AVG(r.bids) AS average, COUNT(r.request_id) AS num, c.species AS species
+                        $query4 = "SELECT k.species, u.name, u.email, k.average, k.num
+                       FROM (SELECT r.taker_id AS id, (SUM(r.bids)/SUM(r.totaltime)*60) AS average, (SUM(r.totaltime)/60) AS num, c.species AS species
                              FROM request r, pet p, petcategory c
-                             WHERE r.pets_id = p.pets_id AND p.pcat_id = c.pcat_id
+                             WHERE r.pets_id = p.pets_id AND p.pcat_id = c.pcat_id AND r.status = 'successful'
                              GROUP BY c.species, r.taker_id) AS k, pet_user u
                        WHERE u.user_id = k.id AND NOT EXISTS(SELECT *
-                                                             FROM (SELECT AVG(r1.bids) AS avg 
+                                                             FROM (SELECT (SUM(r1.bids)/SUM(r1.totaltime)*60) AS avg 
                                                                    FROM request r1, pet p1, petcategory c1 
-                                                                   WHERE r1.pets_id = p1.pets_id AND p1.pcat_id = c1.pcat_id AND c1.species = k.species
+                                                                   WHERE r1.pets_id = p1.pets_id AND p1.pcat_id = c1.pcat_id AND c1.species = k.species AND r1.status = 'successful'
                                                                    GROUP BY r1.taker_id) AS k1 
                                                              WHERE k.average < k1.avg);";
 
-                                $result4 = pg_query($query4) or die('Query failed: ' . pg_last_error());
+                        $result4 = pg_query($query4) or die('Query failed: ' . pg_last_error());
 
-                                while ($row4 = pg_fetch_row($result4)) {
-                                    $average4 = $row4[3] < 0 ? '' : round(floatval($row4[3]), 2);
-                                    echo "
+                        while ($row4 = pg_fetch_row($result4)) {
+                            $average4 = $row4[3] < 0 ? '' : round(floatval($row4[3]), 2);
+                            $totaltime4 = $row4[4] < 0 ? '' : round(floatval($row4[4]), 2);
+                            echo "
                     <tr>
                     <td>$row4[0]</td>
                     <td>$row4[1]</td>
                     <td>$row4[2]</td>
                     <td>$average4</td>
-                    <td>$row4[4]</td>
+                    <td>$totaltime4</td>
                     </tr>";
-                                };
+                        };
 
 
-                                pg_free_result($result2);
-                                ?>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
+                        pg_free_result($result2);
+                        ?>
+                        </tbody>
+                    </table>
                 </div>
-                <div class="row">
-                    <div class="col-sm-12">
-                        <div class="col-sm-6">
-                            <div>
-                                <h4>Takers who have taken care of all species of pets</h4>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-sm-12">
 
-                        <div class="table-vertical first-table">
-                            <table class="table table-striped">
-                                <thead>
-                                <tr>
-                                    <th>Taker Name</th>
-                                    <th>Average Bids Provided</th>
-                                    <th>Number of Successful Assignments Done</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                <?php
-                                $query3 = "SELECT u.name, AVG(r1.bids) AS average, COUNT(r1.taker_id)
+
+                <div class="container">
+                    <h4>Takers who have taken care of all species of pets</h4>
+                </div>
+
+
+                <div class="table-vertical first-table">
+                    <table class="table table-striped">
+                        <thead>
+                        <tr>
+                            <th >Taker Name</th>
+                            <th >Average Bids / Hour</th>
+                            <th >Total Number of Hours Completed</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php
+                        $query3 = "SELECT u.name, (SUM(r1.bids)/SUM(r1.totaltime)*60) AS average, SUM(r1.totaltime)
                        FROM request r1, pet_user u
-                       WHERE r1.taker_id = u.user_id AND NOT EXISTS (SELECT c1.species
-                                                                     FROM petcategory c1
-                                                                     WHERE NOT EXISTS (SELECT *
-                                                                                       FROM request r2, pet p, petcategory c2
-                                                                                       WHERE r2.taker_id = r1.taker_id
-                                                                                             AND r2.pets_id = p.pets_id
-                                                                                             AND p.pcat_id = c2.pcat_id
-                                                                                             AND c2.species = c1.species))
+                       WHERE r1.taker_id = u.user_id AND r1.status = 'successful' AND NOT EXISTS (SELECT c1.species
+                                                                                                  FROM petcategory c1
+                                                                                                  WHERE NOT EXISTS (SELECT *
+                                                                                                                    FROM request r2, pet p, petcategory c2
+                                                                                                                    WHERE r2.taker_id = r1.taker_id
+                                                                                                                    AND r2.pets_id = p.pets_id
+                                                                                                                    AND p.pcat_id = c2.pcat_id
+                                                                                                                    AND c2.species = c1.species
+                                                                                                                    AND r2.status = 'successful'))
                        GROUP BY r1.taker_id, u.name
                        ORDER BY average DESC";
 
-                                $result3 = pg_query($query3) or die('Query failed: ' . pg_last_error());
-                                $flag = 0;
+                        $result3 = pg_query($query3) or die('Query failed: ' . pg_last_error());
+                        $flag = 0;
 
 
-                                while ($row3 = pg_fetch_row($result3)) {
-                                    $flag = 1;
-                                    $average = $row3[1] < 0 ? '' : round(floatval($row3[1]), 2);
-                                    echo "
+                        while ($row3 = pg_fetch_row($result3)) {
+                            $flag = 1;
+                            $average = $row3[1] < 0 ? '' : round(floatval($row3[1]), 2);
+                            $totaltime = $row3[2] < 0 ? '' : round(floatval($row3[1]), 2);
+                            echo "
                     <tr>
                     <td>$row3[0]</td>
                     <td>$average</td>
-                    <td>$row3[2]</td>
+                    <td>$totaltime</td>
                     </tr>";
-                                }
+                        }
 
-                                if (!$flag) {
-                                    echo "<tr><td>No Such Takers Yet</td></tr>";
-                                }
-                                pg_free_result($result3);
-                                ?>
-                                </tbody>
+                        if (!$flag) {
+                            echo "<tr><td>No Such Takers Yet</td></tr>";
+                        }
+                        pg_free_result($result3);
+                        ?>
+                        </tbody>
                             </table>
                         </div>
                     </div>
